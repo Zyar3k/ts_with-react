b445d395c1bc163b82857b17402b3775563f142b
Arkadiusz Zygan <a.zygan@gmail.com>
2021-09-20 17:52:41 +0200

    Advanced props

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
 import "./App.css";
+import Heading from "./components/Heading";
+import Oscar from "./components/Oscar";
 import Greet from "./components/Greet";
-import Person from "./components/Person";
-import PersonList from "./components/PersonList";
+// import Person from "./components/Person";
+// import PersonList from "./components/PersonList";
+import Status from "./components/Status";
 
 function App() {
-  const personName = {
-    first: "John",
-    last: "Wayne"
-  };
+  // const personName = {
+  //   first: "John",
+  //   last: "Wayne"
+  // };
 
-  const nameList = [
-    { first: "John", last: "Wayne" },
-    { first: "Bruce", last: "Wayne" },
-    { first: "Mark", last: "Twain" }
-  ];
+  // const nameList = [
+  //   { first: "John", last: "Wayne" },
+  //   { first: "Bruce", last: "Wayne" },
+  //   { first: "Mark", last: "Twain" }
+  // ];
   return (
     <div className='App'>
-      <Greet name='Alex' messageCount={33} isLoggedIn={false} />
-      <Person name={personName} />
-      <PersonList names={nameList} />
+      <Heading>Lorem, ipsum dolor.</Heading>
+      <Status status='loading' />
+      <Oscar>
+        <Heading>Oscar goes to Mario Puzzo</Heading>
+      </Oscar>
+      <Greet name='Alex' isLoggedIn={true} />
+      {/* <Person name={personName} />
+      <PersonList names={nameList} /> */}
     </div>
   );
 }
